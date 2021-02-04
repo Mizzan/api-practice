ff6adb6ef45c9a5eab46cbdcd8aa2960086b923b
Mizanur Rahman <mizzanur007@gmail.com>
2021-02-04 13:59:50 +0600

    getting random names and user details from an api

---
+++ script.js
@@ -0,0 +1,14 @@
+const button = document.querySelector("button");
+
+button.addEventListener("click", changeName);
+
+function changeName(params) {
+  fetch("https://randomuser.me/api")
+    .then((res) => res.json())
+    .then((data) => {
+      const user = data.results[0];
+      const name = user.name;
+      const userName = `${name.title} ${name.first} ${name.last}`;
+      document.getElementById("user-name").innerText = userName;
+    });
+}
